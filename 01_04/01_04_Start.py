2dc75c39978cc5708cea2d0314f13bbefe37ab68
ossamablt <majidblt0777@gmail.com>
2025-01-08 17:33:25 +0100

    first commit

---
+++ 01_04/01_04_Start.py
@@ -0,0 +1,7 @@
+# Range -> range instance that holds all nums counting by one between 0 and first input
+# List -> lists numbers from the inputted tuple
+belilet=["ossama","ahmed","mohamed","ali","mohamed","ahmed","ali"]
+
+print(list(belilet))
+for i in range(len(belilet)):
+    print("hello mr " + belilet[i])
